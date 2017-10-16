d1981bab584c00a99044ae9d5d3bd799c777badf
Steve Trease <steve@carbon.trease.eu>
2017-10-16 16:41:42 +0100

    Display IP addresses in tableView

--- IP Address/ViewController.swift
+++ IP Address/ViewController.swift
@@ -9,23 +9,51 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+        
+    var interfaces = Interface.allInterfaces()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let interfaces = Interface.allInterfaces()
+        // interfaces = Interface.allInterfaces()
         print (interfaces.count)
-        
-        for interface in interfaces {
-            print (interface.description, interface.address)
-        }
         
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print (interfaces.count)
+        return interfaces.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        
+        print ("cell - \(indexPath.row)")
+        
+        cell.textLabel?.text = interfaces[indexPath.row].description
+        cell.detailTextLabel?.text = interfaces[indexPath.row].address
+        
+        return cell
+    }
+    
+    
+    // screen tap to refresh 
+    @IBAction func screenTappedTriggered(sender: AnyObject) {
+        print (NSURL (fileURLWithPath: "\(#file)").lastPathComponent!, "\(#function)")
+        interfaces = Interface.allInterfaces()
+        print (interfaces.count)
+        self.tableView.reloadData()
     }
 }
