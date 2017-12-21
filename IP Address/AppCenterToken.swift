8a013011ba32df8959803968af603adf4757c045
Steve Trease <steve@carbon.trease.eu>
2017-12-21 13:59:17 +0000

    Checkpoint.

---
+++ IP Address/AppCenterToken.swift
@@ -0,0 +1,12 @@
+//
+//  AppCenterToken.swift
+//  IP Address
+//
+//  Created by Steve on 19/12/2017.
+//  Copyright © 2017 Steve. All rights reserved.
+//
+
+import Foundation
+
+
+public let appCenterToken = "8fb724db-5358-43ca-9d83-e76d88e982a5"
